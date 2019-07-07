ef4669db1beb5efc28e9bf2fc929eddaeefedcfc
James Hotblack <james.c.hotblack@gmail.com>
2019-07-07 16:14:10 +0100

    Second Test. Input txt file can now be parsed and stored in constructor array.

--- spec/data_handlerSpec.js
+++ spec/data_handlerSpec.js
@@ -2,12 +2,25 @@ describe("Data Handler class", function() {
   const DataHandler = require('../src/data_handler')
   var datahandler;
 
+  const dataArray = ["5 5",
+    "1 2",
+    "1 0",
+    "2 2",
+    "2 3",
+   "NNESEESWNWW"]
+
+
   beforeEach(function() {
     datahandler = new DataHandler();
   });
 
   it("Should initialise with empty data array", function() {
-    expect(datahandler.data.length).toEqual(0);
+    expect(datahandler.datastore.length).toEqual(0);
+  });
+
+  it("Should parse text file into data array", function() {
+    datahandler.parse('./input.txt')
+    expect(datahandler.datastore).toEqual(dataArray);
   });
 
 })
--- src/data_handler.js
+++ src/data_handler.js
@@ -1,8 +1,22 @@
+const fs = require('fs');
+
 class DataHandler {
   constructor(){
-    this.data = [];
+    this.datastore = [];
+  }
+
+  parse(input) {
+    var data = fs.readFileSync(input);
+    var filedata = data.toString().split('\n');
+    this.datastore.push(...filedata);   
   }
 }
+
+
+datahandler = new DataHandler();
+datahandler.parse('./input.txt');
+console.log(datahandler.datastore);
+console.log(datahandler.datastore.length);
 
 
 
